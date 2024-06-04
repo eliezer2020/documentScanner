a031c350bef6235423ca9b2cae301c6a5e8c5be5
g0m07km <gerson.eliezer.morales@walmart.com>
2024-06-04 00:53:08 -0600

    added scroll controller

--- lib/pages/documentDetails_widget.dart
+++ lib/pages/documentDetails_widget.dart
@@ -12,6 +12,7 @@ class DocumentDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text('Document Details'),
         ),
         body: Center(
--- lib/widgets/extractedText_widget.dart
+++ lib/widgets/extractedText_widget.dart
@@ -1,32 +1,43 @@
-  import 'dart:io';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 //SafeArea(child: textDisplayer())
 
+Widget textDisplayer(File imagePath) {
+  ScrollController _scrollController = ScrollController();
 
-Widget textDisplayer (File imagePath) {
-   // ignore: unnecessary_null_comparison
-   if( imagePath==null){
-    return const Center(child: Text("No result"),);
-   }else{
-     return FutureBuilder(future: _extractText(imagePath!), 
-     builder: (context, snapshot) {
-return Text(snapshot.data ?? "");
-     });
-   }
+  // ignore: unnecessary_null_comparison
+  if (imagePath == null) {
+    return const Center(
+      child: Text("No result"),
+    );
+  } else {
+    return FutureBuilder(
+        future: _extractText(imagePath!),
+        builder: (context, snapshot) {
+          return Container(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Scrollbar(
+                controller: _scrollController,
+                thickness: 12,
+                thumbVisibility: true,
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Text(snapshot.data ?? ""),
+                ),
+              ));
+        });
   }
-  
- 
-  Future<String?> _extractText(File file)async {
-    final recognizer= TextRecognizer(script:TextRecognitionScript.latin);
-    final inputImage= InputImage.fromFile(file);
-    final extractedText= await recognizer.processImage(inputImage);
-    final text = extractedText.text;
-    recognizer.close();
-    print("===text extracted====== : $text  =====");
-    return text;
+}
 
-
-  }
+Future<String?> _extractText(File file) async {
+  final recognizer = TextRecognizer(script: TextRecognitionScript.latin);
+  final inputImage = InputImage.fromFile(file);
+  final extractedText = await recognizer.processImage(inputImage);
+  final text = extractedText.text;
+  recognizer.close();
+  print("===text extracted====== : $text  =====");
+  return text;
+}
