5a54513c5f673d8b5772601cee78c91ddf832089
g0m07km <gerson.eliezer.morales@walmart.com>
2024-06-05 15:41:29 -0600

    skelethon without document scanner

--- lib/pages/documentDetails_widget.dart
+++ lib/pages/documentDetails_widget.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/widgets/extractedText_widget.dart';
 
+enum PopupList { itemOne, itemTwo }
+
 class DocumentDetails extends StatelessWidget {
   final File imagePath;
 
@@ -12,6 +14,20 @@ class DocumentDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          actions: [
+            PopupMenuButton(
+                itemBuilder: (BuildContext context) =>
+                    <PopupMenuEntry<PopupList>>[
+                      const PopupMenuItem<PopupList>(
+                        value: PopupList.itemOne,
+                        child: Text('send as SQL'),
+                      ),
+                      const PopupMenuItem<PopupList>(
+                        value: PopupList.itemTwo,
+                        child: Text('send as CSV'),
+                      )
+                    ])
+          ],
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text('Document Details'),
         ),
